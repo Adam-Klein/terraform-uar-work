536783d2e02c97b0f9681a2a9bd0bda1cd00ace1
Adam L Klein <alklein@gmail.com>
2020-12-18 14:51:15 -0800

    Added chapter 5 work

---
+++ ch_5/live/global/iam/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region = "us-west-2"
+}
+
+variable "user_names" {
+    description = "Create IAM users with these names"
+    type        = list (string)
+    default     = ["neo", "morpheus"]
+}
+
+resource "aws_iam_user" "example" {
+    for_each    = toset(var.user_names)
+    name        = each.value
+}
+
+output "all_users" {
+    value = aws_iam_user.example
+}
+
+output "all_arns" {
+    value = values(aws_iam_user.example)[*].arn
+}
+
